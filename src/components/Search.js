9989e6dbdea601bee51488768106ee2d80a86780
Adama Faye <46406662+adamafaye1945@users.noreply.github.com>
2024-01-13 14:13:18 -0500

    added search icon

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,24 +1,31 @@
-import { Col, Form,Row } from "react-bootstrap";
+import { Col, Form, Row, InputGroup } from "react-bootstrap";
 import { useAppContext } from "../context/Context";
-import styles from './Search.module.css'
+import styles from "./Search.module.css";
 import AppButton from "./AppButton";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearchengin } from "@fortawesome/free-brands-svg-icons";
 
 function Search() {
-    const {setSearch, search} = useAppContext()
+  const { setSearch, search } = useAppContext();
   return (
     <div>
       <h1>Search books</h1>
       <Form className={styles.form}>
         <Form.Group as={Row}>
           <Form.Label column sm="2">
-            Search Any book by name or Author
+            Search Any book{" "}
           </Form.Label>
           <Col sm="5">
-            <Form.Control
-              placeholder="Books name, Author"
-              value={search}
-              onChange={(e) => setSearch(e.target.value)}
-            />
+            <InputGroup>
+              <InputGroup.Text>
+                <FontAwesomeIcon icon={faSearchengin} />
+              </InputGroup.Text>
+              <Form.Control
+                placeholder="Books name, Author"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+              />
+            </InputGroup>
           </Col>
           <Col sm="5">
             <AppButton type="search">CLEAR SEARCH</AppButton>
